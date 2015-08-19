47a1ad778f1a00d1d4aa5bd86642b94446be4097
Alex Budilovsky <alexbudy@gmail.com>
2015-08-19 12:46:58 -0700

    script + js for team crest svg sizes added

--- generation_scripts/get_all_team_svgs.py
+++ generation_scripts/get_all_team_svgs.py
@@ -2,6 +2,9 @@
 This script is called once to fetch all 20 team svgs and save them to a folder_to_save
 
 Only used to prep the project with team images
+
+Also determines sizes of all svg crests and saves them in javascript
+Pass 'skip_svg_download' as arg1 to only do size analysis
 '''
 
 import requests
@@ -15,10 +18,17 @@ all_teams_url = 'http://api.football-data.org/alpha/soccerseasons/398/teams'
 folder_to_save = '../images/team_icons_svg/'
 skip_SVG_download = False # if arg 'skip_svg_download' passed, only file sizes will be generated from present svg files
 
+team_svg_size_file = '../browser_scripts/team_svg_sizes.js'
+
 def generate_team_names_codes():
 	teams_url = "http://api.football-data.org/alpha/soccerseasons/398/teams"
 
 	teams_data = requests.get(teams_url).json()['teams'] # array of 20 teams
+
+	print("Writing crest SVG width/height to {0}".format(team_svg_size_file))
+	target_size_file = open(team_svg_size_file, 'w')
+	target_size_file.write('// teamCode -> [width, height] (in pixels)\n')
+	target_size_file.write('var team_crest_svg_size = {}')
 
 	for team in teams_data:
 		fullName = team['name']
@@ -44,14 +54,15 @@ def generate_team_names_codes():
 		xmlDoc = minidom.parse(svgFile)
 		svgFile.close()
 
-		print("Parsing " + fileDestination + " XML for width/height ...")
 		svgTag = xmlDoc.getElementsByTagName('svg')[0]
 		width  = ''.join(c for c in str(svgTag.getAttribute('width'))  if (c.isdigit() or c == '.'))
 		height = ''.join(c for c in str(svgTag.getAttribute('height')) if (c.isdigit() or c == '.'))
 		
-		print("w: " + width)
-		print("h: " + height)
+		crestSizeLine = 'team_crest_svg_size[\'{0}\'] = [{1}, {2}]'.format(teamCode, width, height)
+		print("Writing " + crestSizeLine + " to file")
+		target_size_file.write("\n" + crestSizeLine)
 
+	target_size_file.close()
 
 
 if len(sys.argv) > 1:
